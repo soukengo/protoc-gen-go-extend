0ca3a7bd814665d4791080dfc2798434c709f5cf
soukengo <soukengo@163.com>
2022-10-24 20:15:25 +0800

    fix: repeated field

--- setters/value.go
+++ setters/value.go
@@ -10,6 +10,8 @@ import (
 func getFieldType(g *generator.Generator, field *descriptor.FieldDescriptorProto) string {
 	var varType string
 	switch *field.Type {
+	case descriptor.FieldDescriptorProto_TYPE_DOUBLE:
+		varType = "float64"
 	case descriptor.FieldDescriptorProto_TYPE_MESSAGE:
 		varType = getMessageFieldType(g, field)
 	case descriptor.FieldDescriptorProto_TYPE_ENUM:
@@ -17,6 +19,9 @@ func getFieldType(g *generator.Generator, field *descriptor.FieldDescriptorProto
 		varType = g.TypeName(desc)
 	default:
 		varType = strings.ToLower(strings.Split(field.Type.String(), "_")[1])
+	}
+	if isRepeated(field) && !isMapField(g, field) {
+		varType = "[]" + varType
 	}
 	return varType
 }
@@ -30,6 +35,18 @@ func getMessageFieldType(g *generator.Generator, field *descriptor.FieldDescript
 		return fmt.Sprintf("map[%s]%s", keyType, valType)
 	}
 	return "*" + g.TypeName(desc)
+}
+
+func isMapField(g *generator.Generator, field *descriptor.FieldDescriptorProto) bool {
+	desc := g.ObjectNamed(field.GetTypeName())
+	if d, ok := desc.(*generator.Descriptor); ok && d.GetOptions().GetMapEntry() {
+		return true
+	}
+	return false
+}
+
+func isRepeated(field *descriptor.FieldDescriptorProto) bool {
+	return field.Label != nil && *field.Label == descriptor.FieldDescriptorProto_LABEL_REPEATED
 }
 
 func upperFirstLatter(s string) string {
